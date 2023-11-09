829766dad4acde745393587b9631f93965df6591
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-09 21:13:23 +0300

    Create 03-first-look.py

---
+++ 03-first-look.py
@@ -0,0 +1,12 @@
+# Use the head() function to display the first 5 rows of the data
+# We can use head(10), head(12) etc...
+data.head()
+
+# Use the tail() function to display the first 5 rows of the data
+# We can use tail(10), tail(12) etc...
+data.tail()
+
+# Print the shape of the data and classes
+print(data.shape)
+
+print(data['Class'].unique())
