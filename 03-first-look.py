3dcccdbe907985d5b07aa341d6e745b449cef163
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-09 21:13:55 +0300

    Update 03-first-look.py

--- 03-first-look.py
+++ 03-first-look.py
@@ -2,7 +2,7 @@
 # We can use head(10), head(12) etc...
 data.head()
 
-# Use the tail() function to display the first 5 rows of the data
+# Use the tail() function to display the last 5 rows of the data
 # We can use tail(10), tail(12) etc...
 data.tail()
 
