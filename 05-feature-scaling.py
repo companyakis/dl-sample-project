b40d96ed737048bce3cb16982dbc9c2b1bfb1ecd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-09 21:20:21 +0300

    Create 05-feature-scaling.py

---
+++ 05-feature-scaling.py
@@ -0,0 +1,8 @@
+# scale all of the values between the 0 and 1 range
+# features in different units or ranges can be problematic
+
+# Normalize the features dataset and assign it to a variable
+X_scaled = minmax_scale(X)
+
+# Create a DataFrame using the new variable
+X = pd.DataFrame(X_scaled)
