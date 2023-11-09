4576e3230528cc2cae0035aee49f23ecd4c61c01
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-09 21:09:18 +0300

    Create 01-importing-required-libraries.py

---
+++ 01-importing-required-libraries.py
@@ -0,0 +1,8 @@
+# Import Pandas and Matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Import Label Encoder and train_test_split
+from sklearn.preprocessing import LabelEncoder, minmax_scale
+from sklearn.model_selection import train_test_split
