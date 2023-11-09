7ff5f75badb96ee9ff556a2ef9e298ce4381c1ac
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-09 21:37:28 +0300

    Create 09-input-layer.py

---
+++ 09-input-layer.py
@@ -0,0 +1,8 @@
+# Construct an input layer and assign it to a variable
+# The first argument is the number of nodes we want in that hidden layer
+
+# Only for the input layer, we have to set the input_shape argument which is the number of columns (# of X columns)
+# in this case, 24
+
+# Create an input layer
+input_layer = tf.keras.layers.Dense(4096, input_shape=(24,), activation="relu")
